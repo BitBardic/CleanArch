19b7dc9bf0546a3f9e2c03bcfaac8953cbc0e922
Nasir Iqbal <nasirpucit@hotmail.com>
2020-05-16 06:01:00 +0100

    Added Api Project

---
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+    }
+}
